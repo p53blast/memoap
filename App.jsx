e466906c8a91d27d0e33e757993412471db5259e
46290085+p53blast@users.noreply.github.com <damepare@yahoo.co.jp>
2022-09-23 11:23:42 +0900

    Create Hello Component

--- App.jsx
+++ App.jsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Hello from './src/components/Hello';
+
 export default function App() {
+
+  //機能
   return (
     <View style={styles.container}>
+      <Hello bang={false}>Japan</Hello>
+      <Hello bang style = {{fontSize : 16}}>India</Hello>
       <Text>Open up App.js to start working on your app!</Text>
+      {/* eslint-disable-next-line */}
       <StatusBar style="auto" />
     </View>
   );
